82e690ee001bd2dcf062cecd98d95bde9b7cda55
boo314 <boo314@gmail.com>
2014-04-27 14:01:37 +0200

    init

---
+++ jq/myscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('#imie').blur(function(){
+		$(this).myvalidate('required',{'errorText':'Pole imie jest wymagane','messageBox':'bledy'});
+	});
+	$('#nazwisko').blur(function(){
+		$(this).myvalidate('bigcapital',{'errorText':'Nazwisko musi zaczynac sie wielka litera'});
+	});
+	$('#yob').blur(function(){
+		$(this).myvalidate('isNumber');
+	});
+});
